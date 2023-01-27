40c7d9ff2a8540a7c0214a93d9bd5460e63c2526
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-01-27 13:19:40 +0530

    Add : Modify the earlier UC 2 to continue till either of them have won 21 times. Show if it’s a Win or Tie. If Win then who won and by how much

--- FlipCoinSimulationProblem.sh
+++ FlipCoinSimulationProblem.sh
@@ -1,27 +1,35 @@
 echo "Welcome To Flip Coin Simulation Problem "
 
-read -p "Enter a number how many times u want to toss : " n
+target=21
 heads=0
 tails=0
-for(( i = 1; i <= n; i++ ))
+flips=0
+while(( 1 ))
 do
+    (( flips_count++ ))
+    echo -n "Flip-$flips is "
     toss=$(( RANDOM % 2 ))
     if(( toss == 0 ))
     then
-        echo "Flip-$i Heads"
+        echo "Heads"
         (( heads++ ))
     else
-        echo "Flip-$i Tails"
+        echo "Tails"
         (( tails++ ))
+    fi
+    if(( heads == target || tails == target ))
+    then
+        break
     fi 
 done
 echo "The Heads count is $heads and Tails Count is $tails"
 if(( heads > tails ))
 then
-    echo "Winner is Heads"
+    echo "Heads won by $(( heads - tails ))"
 elif(( tails > heads ))
 then
-    echo "Winner is Tails"
+    echo "Tails won by $(( tails - heads ))"
 else    
-    echo "Toss Again"
+    echo "Its tie"
 fi
+
